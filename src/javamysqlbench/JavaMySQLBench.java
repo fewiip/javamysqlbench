38104318222e8f6661296fd08981449ec034ec55
Felipe Avelino <avelino.felipe@gmail.com>
2022-05-10 23:06:50 -0400

    transicao de telas funcionando

---
+++ src/javamysqlbench/JavaMySQLBench.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javamysqlbench;
+
+/**
+ *
+ * @author felipe
+ */
+public class JavaMySQLBench {
+    
+    public static String login;
+    public static String password;
+    public static String database;
+    
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}
--- src/javamysqlbench/NewJFrame2.java
+++ src/javamysqlbench/NewJFrame2.java
@@ -24,22 +24,16 @@ public class NewJFrame2 extends javax.swing.JFrame {
     private Statement stmt;
     private ResultSet rs;
     private String query; 
+    
+    private String login;
+    private String password;
+    private String database;
 
     /**
      * Creates new form NewJFrame2
      */
     public NewJFrame2() {
-        
-        try {
-            String url = "jdbc:mysql://localhost/test_db";
-            String user = "root";
-            String password = "felipinho@Fedora123";
-            con = DriverManager.getConnection(url, user, password);
-        }catch(SQLException sqle){
-            System.out.println("erro bd: "+sqle);
-        }
-        
-        
+                
         initComponents();
     }
 
@@ -139,6 +133,21 @@ public class NewJFrame2 extends javax.swing.JFrame {
         // TODO add your handling code here:
         
         // TODO add your handling code here:
+        try {
+            login = JavaMySQLBench.login;
+            password = JavaMySQLBench.password;
+            database = JavaMySQLBench.database;
+            
+            String url = "jdbc:mysql://localhost/"+database;//"test_db";
+            /*
+            String user = "root";
+            String password = "felipinho@Fedora123";*/
+            con = DriverManager.getConnection(url, login, password);
+        }catch(SQLException sqle){
+            System.out.println("erro bd: "+sqle);
+        }
+        
+        
         query = this.TextAreaQuery.getText();
                 
         try {
@@ -191,37 +200,6 @@ public class NewJFrame2 extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new NewJFrame2().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton ExecuteButton;
